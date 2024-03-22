2811d02c87edc34980b655342d3cd65ffde4baa0
Kauã Otaviano <138393850+Otaviano00@users.noreply.github.com>
2024-03-22 16:04:06 -0300

    Update Program.java

--- Program/src/br/com/visao/Program.java
+++ Program/src/br/com/visao/Program.java
@@ -1,14 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.com.visao;
 
-/**
- *
- * @author 355828
- */
+import br.com.controle.Cliente;
+import br.com.controle.ContraCheque;
+import br.com.controle.Loja;
+import br.com.controle.Contrato;
+import br.com.controle.Morador;
+import br.com.controle.NotaFiscal;
+import br.com.controle.Porteiro;
+import br.com.controle.Produto;
+import br.com.controle.Vendedor;
+import br.com.controle.Visitante;
+
 public class Program {
-    
+    public static void main(String[] args) {
+        Loja loja1 = 
+                new Loja(
+                        123456,
+                        "Riachuelo",
+                        new Cliente(
+                                "082.429.551-55",
+                                "Kauã Otaviano Teixeira",
+                                new NotaFiscal(
+                                        987654,
+                                        155.69,
+                                        new Produto(
+                                                159753,
+                                                "Queijo",
+                                                155.69
+                                        )
+                                )
+                        ),
+                        new Vendedor(
+                                10.0,
+                                "Rogério Cleiton",
+                                new ContraCheque(
+                                        "17/10/2016",
+                                        15.69
+                                )
+                        ),
+                        new Produto(
+                                159753,
+                                "Queijo",
+                                155.69
+                        ),
+                        new Contrato(
+                                357951,
+                                "Um contrato muito foda",
+                                1000.69
+                        )
+                );
+    }
 }
