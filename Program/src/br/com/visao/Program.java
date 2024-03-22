b2ac3ccf0b131b29b2c170132f58343f29c08d85
IcaroGRF201 <141679674+IcaroGRF201@users.noreply.github.com>
2024-03-22 16:04:54 -0300

    Update Program.java

--- Program/src/br/com/visao/Program.java
+++ Program/src/br/com/visao/Program.java
@@ -1,19 +1,30 @@
 
 package br.com.visao;
 
-import br.com.controle.Cliente;
+import br.com.controle.Apartamento;
 import br.com.controle.ContraCheque;
-import br.com.controle.Loja;
 import br.com.controle.Contrato;
 import br.com.controle.Morador;
-import br.com.controle.NotaFiscal;
 import br.com.controle.Porteiro;
+import br.com.controle.Visitante;
+import br.com.controle.Cliente;
+import br.com.controle.Loja;
+import br.com.controle.NotaFiscal;
+import br.com.controle.Pessoa;
 import br.com.controle.Produto;
 import br.com.controle.Vendedor;
-import br.com.controle.Visitante;
 
 public class Program {
     public static void main(String[] args) {
+        Apartamento apartamento1 = 
+               new Apartamento (1234,
+                       4321,
+                       new Porteiro ("Noturno", 5000,
+                       new ContraCheque ("29/03/2024", 5000),
+                       "Cleber"),
+                       new Contrato (356214, "Uma boa pessoa", 5000),
+                       new Visitante ("87654321", "SamambaiaSul", "Alberto"),
+                       new Morador ("87654321", "Davi"));
         Loja loja1 = 
                 new Loja(
                         123456,
